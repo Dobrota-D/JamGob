a616c2ec7c8588c0b9bebf4833d0578f1402f7c4
Dobrota <dobrotakevin@yahoo.fr>
2024-01-10 15:24:18 +0100

    Fix explosion intégration lobby in main

--- Assets/Scripts/ExplosiveCane.cs
+++ Assets/Scripts/ExplosiveCane.cs
@@ -5,6 +5,42 @@ using UnityEngine;
 public class ExplosiveCane : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public GameObject explosion;
+    public float radius = 5.0F;
+    public float power = 10.0F;
+
+    //if the tag ball hit the object, it will explode
+
+    private void OnTriggerEnter(Collider other)
+    {
+       if (other.gameObject.CompareTag("ball"))
+        {
+            Debug.Log("Explosion");
+            //make the explosion with the radius and power of the explosion
+            Vector3 explosionPosition = transform.position;
+            Collider[] colliders = Physics.OverlapSphere(explosionPosition, radius);
+            foreach (Collider hit in colliders)
+            {
+                Debug.Log(hit, hit);
+                Rigidbody rb = hit.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    //add force to the object
+                    Vector3 direction = (hit.transform.position - transform.position) * power;
+                    rb.AddForce(direction, ForceMode.VelocityChange);
+                }
+            }
+
+            //destroy the object
+
+            Destroy(explosion);
+
+
+        }
+    }
+    
+
     void Start()
     {
         
